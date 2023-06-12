6b95c5bfdb0b88dd4e1c67a886fddce720ab67df
darsh <darshngaikwad2499@gmail.com>
2023-06-12 23:12:02 +0530

    darshan

---
+++ JavaScript/Basic of js/hello.js
@@ -0,0 +1,3 @@
+console.log("hello GFG");
+console.log("darshan sharad gaikwad");
+console.log(7+3);
---
+++ JavaScript/Basic of js/variable.js
@@ -0,0 +1,13 @@
+var message;
+message="Hello Darshan";
+//console.log(message);
+message="MAMA ALA NA BHAI";
+console.log(message);
+
+let txet;
+ txet="Javascript is best"
+console.log(txet);
+
+
+//const num;
+//num=10;
